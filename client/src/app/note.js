1684856323cd2665c6df149fab7ddc37f36360a3
przemekijak <przemekijak@gmail.com>
2020-01-05 12:40:11 +0100

    note refactored, optimization fine

--- client/src/app/note.js
+++ client/src/app/note.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/note.css';
 import Current from './note/current'
 import Sold from './note/sold'
@@ -15,19 +15,15 @@ import {
     Switch
   } from 'react-router-dom';
 
-class Render extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            deleteMode: false,
-            itemModal: false,
-            currentItem: 0,
-        };
-    }
-    toggleDelete = () => {
-        this.setState(prevState => ({
-            deleteMode: !prevState.deleteMode
-        }));
+
+  function Render(props) {
+        const [deleteMode, setDeleteMode] = useState(false);
+        const [itemModal, setItemModal] = useState(false);
+        const [currentItem, setCurrentItem] = useState(0);
+        const socket = props.socket;
+
+    function toggleDelete(){
+        setDeleteMode(!deleteMode);
                             // eslint-disable-next-line
                             if($(".deleteButton").css('opacity') == 0) {
                                 $(".deleteButton").css('opacity', 1);
@@ -38,20 +34,19 @@ class Render extends React.Component {
                             }
     }
 
-       deleteItem = (id) => {
-           this.props.socket.emit('deleteItem', id.target.id)
-           this.props.refreshItems();
-           this.toggleDelete();
+       function deleteItem(id) {
+           socket.emit('deleteItem', id.target.id)
+           props.refreshItems();
+           toggleDelete();
        }
-       itemInfo = (id) => {
-            this.setState({currentItem: id});
-            this.setState({itemModal: true});
-       }
-       handleModal() {
-           this.setState({itemModal: !this.state.itemModal});
+       function itemInfo(id) {
+            setCurrentItem(id);
+            setItemModal(true);
+        }
+       function handleModal() {
+           setItemModal(!itemModal);
        }
 
-    render() {
         return (
             <Router>
                  <div className="tableContainer">
@@ -73,27 +68,26 @@ class Render extends React.Component {
                     <div className="noteContent">
                         <Switch>
                             <Route path="/note/sold">
-                              <Sold items={this.props.soldItems} deleteItem={this.deleteItem} itemInfo={(id) => this.itemInfo(id)}/>
+                              <Sold items={props.soldItems} deleteItem={deleteItem} itemInfo={(id) => itemInfo(id)}/>
                             </Route>
                             <Route path="/note/pending">
                                 <Pending/>
                             </Route>
                             <Route path="/">
-                             <Current socket={this.props.socket} itemInfo={(id) => this.itemInfo(id)} items={this.props.currentItems} deleteItem={this.deleteItem} refreshItems={this.props.refreshItems}/>
+                             <Current socket={socket} itemInfo={(id) => itemInfo(id)} items={props.currentItems} deleteItem={deleteItem} refreshItems={props.refreshItems}/>
                             </Route>
                         </Switch>
                     </div>
-                    <NoteMenu socket={this.props.socket} userID={this.props.userID} deleteMode={this.toggleDelete} refreshItems={this.props.refreshItems}/>
+                    <NoteMenu socket={socket} userID={props.userID} deleteMode={toggleDelete} refreshItems={props.refreshItems}/>
 
-                    <ReactModal isOpen={this.state.itemModal} className={"modalContent"} overlayClassName={"modalOverlay"} onRequestClose={() => this.setState({itemModal: false})}>
-                    <ItemInfo socket={this.props.socket} handleModal={this.handleModal} itemID={this.state.currentItem}/>
+                    <ReactModal isOpen={itemModal} className={"modalContent"} overlayClassName={"modalOverlay"} onRequestClose={() => setItemModal(false)}>
+                    <ItemInfo socket={socket} handleModal={handleModal} itemID={currentItem}/>
             </ReactModal>
 
                 </div>
             </Router>
         )
     }
-}
 
 export default {
     Render
