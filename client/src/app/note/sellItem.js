0d29f5156086527143a6e76f62a55fdb4dd24c09
przemekijak <przemekijak@gmail.com>
2020-01-05 19:01:19 +0100

    sell item validation data

--- client/src/app/note/sellItem.js
+++ client/src/app/note/sellItem.js
@@ -5,6 +5,7 @@ function SellItem(props) {
     const formBox = useRef();
     const itemPrice = useRef();
     const soldFor = useRef();
+    const sellData = [itemPrice, soldFor];
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -13,10 +14,17 @@ function SellItem(props) {
             price: itemPrice.current.value,
             soldFor: soldFor.current.value,
         }
-        // Checking if price is a number
-        if(!isNaN(item.price))
-        {
-            socket.emit('sellItem',item);
+        // validate data
+        var validateData = 0;
+        for(var element of sellData) {
+            if(/^[a-zA-Z0-9 ]+$/.test(element.current.value)) {
+                validateData++;
+                if(validateData === sellData.length) {
+                 if(!isNaN(item.price)) {
+                    socket.emit('sellItem',item);
+                    }
+                }
+            }
         }
         props.refreshItems();
         props.handleModal();
