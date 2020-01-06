17df4d8970ba6156d5c153a70df37bd70dd0308b
przemekijak <przemekijak@gmail.com>
2020-01-06 17:52:12 +0100

    serv update heroku

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ app.listen(port, () => console.log(`Hypehub running on port ${port}`));
 app.use(bodyParser.json());
 // app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-//   });
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
 
 // handle sesssion
 app.use(session);
