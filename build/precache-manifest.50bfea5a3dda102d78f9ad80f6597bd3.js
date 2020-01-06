b158a2036b17fa2527224557cbfd286bf1085d94
przemekijak <przemekijak@gmail.com>
2020-01-06 19:48:55 +0100

    heroku

---
+++ build/precache-manifest.50bfea5a3dda102d78f9ad80f6597bd3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "742523d5fd01b6c4e284785c84143948",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f8f3bdbddb72a179477",
+    "url": "/static/css/main.8ca00b4a.chunk.css"
+  },
+  {
+    "revision": "a4fb69e5304099a46846",
+    "url": "/static/js/2.93491d84.chunk.js"
+  },
+  {
+    "revision": "8f8f3bdbddb72a179477",
+    "url": "/static/js/main.c8bd024c.chunk.js"
+  },
+  {
+    "revision": "58eb1be3311be8fd2a4c",
+    "url": "/static/js/runtime-main.95d2a168.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0a020e367f7aa758caff2b01c27bb6db.js"
+  "/precache-manifest.50bfea5a3dda102d78f9ad80f6597bd3.js"
 );
 
 self.addEventListener('message', (event) => {
