0db68853e6a4f35fa2c5b8975fcd66544efee0c9
przemekijak <przemekijak@gmail.com>
2020-01-06 18:56:21 +0100

    test

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname+'./client/build/index.html'));
 });
 
 // handle sesssion
