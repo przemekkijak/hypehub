c51ee2330852e06496e9ca1873cc0864f22db7fb
przemekijak <przemekijak@gmail.com>
2020-02-02 17:36:52 +0100

    Estimated profit in itemInfo

--- client/src/app/note/itemInfo/info.js
+++ client/src/app/note/itemInfo/info.js
@@ -7,8 +7,9 @@ function Info(props) {
         <div className="infoContainer">
             <p id="itemName"> {props.item.name} </p>
             <div className="itemDetails">
-            <span>Kupiono za: {props.item.buyPrice} zł</span>
-            <span>Planowana sprzedaż: {props.item.estimatedPrice} zł</span>
+            {/* <span>Kupiono za: {props.item.buyPrice} zł</span>
+            <span>Planowana sprzedaż: {props.item.estimatedPrice} zł</span> */}
+            <span>Przewidywany zarobek: {props.item.estimatedPrice-props.item.buyPrice} zł</span>
             <span>
                 Rozmiar: {props.item.size}
                 {props.item.type === 2 && (
