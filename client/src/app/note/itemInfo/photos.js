f750c7b3f02a6e49ba25187ce0f4b385067e4eaf
przemekijak <przemekijak@gmail.com>
2020-02-17 14:19:27 +0100

    positioning update

--- client/src/app/note/itemInfo/photos.js
+++ client/src/app/note/itemInfo/photos.js
@@ -1,46 +1,9 @@
 import React, {useEffect, useState, useRef} from 'react'
 import '../../styles/photos.css';
-import SocketIOFileUpload from 'socketio-file-upload'
 
 function Photos(props) {
-    const uploader = new SocketIOFileUpload(props.socket);
-    const [reload,setReload] = useState(false);
-    const order = useRef(1);
 
 
-    useEffect(() => {
-        console.log('Mountend with item: ' + props.item.name);
-        uploader.listenOnInput(document.getElementById('photoFile'));
-        for(let i = 1; i<=4; i++ ) {
-                props.socket.emit('checkPhoto', props.item.id, i, found => {
-                    if(found) {
-                        document.getElementById(i).setAttribute("src", `/img/items/${props.item.id}_${i}.jpg`)
-                    } else {
-                        document.getElementById(i).setAttribute("src", `/img/items/nophoto.jpg`);
-                        document.getElementById(i).onclick = () => {
-                            order.current = i;
-                            document.getElementById('photoFile').click();
-                        }
-                    }
-                });
-        }
-        return () => {
-            console.log('unmount');
-        }
-    }, [reload]);
-
-    props.socket.once('file_saved', fn => {
-        let itemData = {
-            id: props.item.id,
-            order: order.current
-        }
-        fn(itemData);
-    })
-
-    props.socket.once('photoComplete', () => {
-        uploader.destroy();
-        setReload(!reload);
-    })
 
 return (
     <div className="photoContainer">
@@ -50,7 +13,6 @@ return (
             <form>
             <input type="file" id="photoFile"/>
             </form>
-            {/* <button id="uploadPhoto" onClick={() => uploadPhoto()}>Upload</button> */}
         </div>
 
         <div className="photosBox">
