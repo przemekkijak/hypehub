7cfbb141dec47d4532f951512ae8f4fdb3d211b0
przemekijak <przemekijak@gmail.com>
2020-02-27 14:14:59 +0100

    merging photos without copy to clipboard - todo

--- client/src/app/note/itemInfo/photos.js
+++ client/src/app/note/itemInfo/photos.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useRef} from 'react'
 import '../../styles/photos.css';
 import SocketIOFileUpload from 'socketio-file-upload';
+import mergeImages from 'merge-images';
+
 
 
 
@@ -40,6 +42,21 @@ uploader.addEventListener('start', (event) => {
     });
 
 
+    function merge() {
+
+        mergeImages([
+            {src: `/img/items/but1.jpg`, x:0,y: 0},
+            {src: `/img/items/but2.jpg`, x:1000,y:0},
+            {src: `/img/items/but3.jpg`,x:0,y:1250},
+            {src: `/img/items/but4.jpg`, x:1000,y:1250},
+        ], {
+            width: 4000,
+            height: 5000
+        }).then(b64 => {
+            document.getElementById('testuje').src = b64;
+            });
+    }
+
 return (
     <div className="photoContainer">
 
@@ -51,11 +68,12 @@ return (
             <div className="photo"><img src="" alt="Third" id="3"/></div>
             <div className="photo"><img src="" alt="Fourth" id="4"/></div>
         </div>
-        <div className="photosOptions">
+        <div className="photosOptions" id="options">
             <p>Za pomocą poniższych przycisków w łatwy sposób możesz wygenerować gotowe zdjęcie do wystawienia przedmiotu na sprzedaż. Aplikacja automatycznie zlepi wszystkie cztery zdjęcia i skopiuje do Twojego schowka efekt końcowy.
                 <br/>Dostępne są dwa warianty, jeden w postaci czystych zdjęć, oraz drugi z dodatkowymi informacjami o Twoim przedmiocie. </p>
-            <button>Zdjęcie z opisem</button>
+            <button onClick={() => merge()}>Zdjęcie z opisem</button>
             <button>Zdjęcie bez opisu</button>
+            <img src='/img/items/nophoto.jpg' id='testuje'/>
         </div>
     </div>
 
