ffd3d2fbefc466ea4992efc047a359a9f358e5e5
przemekijak <przemekijak@gmail.com>
2020-03-20 13:56:23 +0100

    item info deleted, border-radius to sellItem and modifyItem

--- client/src/app/note/itemInfo/info.js
+++ client/src/app/note/itemInfo/info.js
@@ -4,23 +4,6 @@ import '../../styles/info.css';
 
 function Info(props) {
 
-    function copyDesc() {
-        let description;
-        if(props.item.type === 1) {
-            description = `${props.item.name}\n${props.item.cond}/10 \n${props.item.size} (${props.item.length}/${props.item.width})\n${props.item.estimatedPrice}PLN`;
-        } else {
-            description = `${props.item.name}\n${props.item.cond}/10 \n${props.item.size}\n${props.item.estimatedPrice}PLN`;
-        }
-        const area = document.createElement('textarea');
-        area.value = description;
-        area.setAttribute('readonly','');
-        area.style.position = 'absolute';
-        area.style.left = '-999999px';
-        document.body.appendChild(area);
-        area.select();
-        document.execCommand('copy');
-        document.body.removeChild(area);
-    }
     function unSold() {
         axios.post('https://hypehub.pl/unSold', {
             id: props.item.id
