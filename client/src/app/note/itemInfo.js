fbf7f2f9fcb74686b3d54f93e2477240eddf6ad4
przemekijak <przemekijak@gmail.com>
2020-03-20 13:56:23 +0100

    item info deleted, border-radius to sellItem and modifyItem

--- client/src/app/note/itemInfo.js
+++ client/src/app/note/itemInfo.js
@@ -8,7 +8,6 @@
 function ItemInfo(props) {
   const [loaded, setLoaded] = useState(false);
   const [item, setItem] = useState();
-  const [menu, setMenu] = useState(1);
 
   useEffect(() => {
     !loaded &&
@@ -24,46 +23,13 @@ function ItemInfo(props) {
   });
 
 
-  function itemMenu(menu) {
-    switch(menu) {
-      default:
-      case 1:
-        return <Info item={item} handleModal={props.handleModal} refreshItems={props.refreshItems}/>
-      case 2:
-        return <Modify item={item} handleModal={props.handleModal} refreshItems={props.refreshItems}/>
-    }
-  }
-
   return (
     loaded && (
       <div className="itemInfoContainer">
-      <div className="itemMenu">
-        <input
-          type="radio"
-          name="itemoption"
-          id="info"
-          className="detailsRadio"
-          onChange={() => setMenu(1)}
-          defaultChecked
-        />
-        <label htmlFor="info">
-          Informacje
-        </label>
 
-        <input
-          type="radio"
-          name="itemoption"
-          id="edit"
-          className="detailsRadio"
-          onChange={() => setMenu(2)}
-        />
-        <label htmlFor="edit">
-          Edytuj
-        </label>
 
-      </div>
         <div className="itemContent">
-          {itemMenu(menu)}
+          <Modify item={item} handleModal={props.handleModal} refreshItems={props.refreshItems}/>
         </div>
 
       </div>
