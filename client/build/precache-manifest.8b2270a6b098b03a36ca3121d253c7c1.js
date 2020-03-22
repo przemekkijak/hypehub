e6a56a2838480dd1ff2f92b9632e0472a5770b56
przemekijak <przemekijak@gmail.com>
2020-03-22 18:03:15 +0100

    gitignore added after so much time...

---
+++ client/build/precache-manifest.8b2270a6b098b03a36ca3121d253c7c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3cfe727fedd0484d82e2daf8ba06cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07bf59311d15f103ea64",
+    "url": "/static/css/main.7aee6d85.chunk.css"
+  },
+  {
+    "revision": "3576fc523d7e6a13a588",
+    "url": "/static/js/2.18307e71.chunk.js"
+  },
+  {
+    "revision": "07bf59311d15f103ea64",
+    "url": "/static/js/main.f95c0643.chunk.js"
+  },
+  {
+    "revision": "58eb1be3311be8fd2a4c",
+    "url": "/static/js/runtime-main.95d2a168.js"
+  }
+]);
