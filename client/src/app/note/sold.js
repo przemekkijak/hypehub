1f7c4105fe2f2e030c3230d206b232f65c0a611a
przemekijak <przemekijak@gmail.com>
2020-03-23 12:55:10 +0100

    item for 0 items added

--- client/src/app/note/sold.js
+++ client/src/app/note/sold.js
@@ -5,8 +5,10 @@ function Sold(props) {
   function deleteItem(id) {
     axios.post(`https://hypehub.pl/deleteItem`, {
       id: id,
-    });
-    props.refreshItems();
+    })
+    .then(() => {
+      props.refreshItems();
+    })
   }
 
   function convertCondition(cond) {
@@ -52,38 +54,42 @@ function Sold(props) {
   }
 
   return (
-    <div className="soldContainer">
-      {props.items.map((item, index) => (
-        <div className="itemSlot soldColumns" id="" key={index}>
-          <p onClick={() => props.itemInfo(item.id)}>{item.name}</p>
-          <p onClick={() => props.itemInfo(item.id)}>{itemSize(item)}</p>
-          <p onClick={() => props.itemInfo(item.id)}>
-            {convertCondition(item.cond)}
-          </p>
-          <p onClick={() => props.itemInfo(item.id)}>{item.buyPrice} zł</p>
-          <p onClick={() => props.itemInfo(item.id)} id="earnings">
-            {item.sellPrice - item.buyPrice} zł
-          </p>
-          <p onClick={() => props.itemInfo(item.id)}>{getTracking(item)}</p>
-          <p onClick={() => props.itemInfo(item.id)}>{item.soldFor}</p>
-          <p>
-            <button
-              className="actionButton"
-              onClick={() => {
-                if (window.confirm(`Czy napewno usunąć ${item.name}?`))
-                  deleteItem(item.id);
-              }}
-            >
-              <img
-                src="/img/note/delete.png"
-                alt="delete"
-                className="noteIcon"
-              />
-            </button>
-          </p>
-        </div>
-      ))}
-    </div>
+    <>
+    {props.items.length == 0 ? <p id="noItems">Nie posiadasz jeszcze żadnych przedmiotów</p> :
+      <div className="soldContainer">
+        {props.items.map((item, index) => (
+          <div className="itemSlot soldColumns" id="" key={index}>
+            <p onClick={() => props.itemInfo(item.id)}>{item.name}</p>
+            <p onClick={() => props.itemInfo(item.id)}>{itemSize(item)}</p>
+            <p onClick={() => props.itemInfo(item.id)}>
+              {convertCondition(item.cond)}
+            </p>
+            <p onClick={() => props.itemInfo(item.id)}>{item.buyPrice} zł</p>
+            <p onClick={() => props.itemInfo(item.id)} id="earnings">
+              {item.sellPrice - item.buyPrice} zł
+            </p>
+            <p onClick={() => props.itemInfo(item.id)}>{getTracking(item)}</p>
+            <p onClick={() => props.itemInfo(item.id)}>{item.soldFor}</p>
+            <p>
+              <button
+                className="actionButton"
+                onClick={() => {
+                  if (window.confirm(`Czy napewno usunąć ${item.name}?`))
+                    deleteItem(item.id);
+                }}
+              >
+                <img
+                  src="/img/note/delete.png"
+                  alt="delete"
+                  className="noteIcon"
+                />
+              </button>
+            </p>
+          </div>
+        ))}
+      </div>
+    }
+  </>
   );
 }
 
